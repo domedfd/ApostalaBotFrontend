d11182da271066be4221cfbda37253282f693d3a
Domenico Delvalle <domedfd@gmail.com>
2020-06-15 23:48:06 -0400

    change default url api backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "https://apostala-b.herokuapp.com",
+  baseURL: process.env.URLAPI || "https://localhost:3333",
 });
 
 export default api;
